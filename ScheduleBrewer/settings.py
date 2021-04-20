bfad4fc5ccf05d3da31e85d984feb5f912e80873
leeyuchong <leeyuchong@outlook.com>
2021-04-20 12:41:20 -0400

    Updated settings.py

--- ScheduleBrewer/settings.py
+++ ScheduleBrewer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-54-83-17-12.compute-1.amazonaws.com', '54.83.17.12', 'localhost', 'schedulebrewer.ml', 'www.schedulebrewer.ml']
+ALLOWED_HOSTS = ['ec2-54-83-17-12.compute-1.amazonaws.com', '54.83.17.12', 'localhost', 'schedulebrewer.ml', 'www.schedulebrewer.ml', '127.0.0.1']
 
 
 # Application definition
