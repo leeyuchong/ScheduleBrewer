332a0bca7a8be22f5a503f7d63afd3e5e3fcba9a
leeyuchong <leeyuchong@outlook.com>
2021-04-21 11:00:16 -0400

    Modified views and urls for saml

--- ScheduleBrewer/secretKey.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def get_random():
-    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    return ''.join(random.choice(chars) for _ in range(50))
-
-print(get_random())
