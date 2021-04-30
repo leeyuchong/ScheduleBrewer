12599170e62ecfed9a0e89f685e5d6f0b78da8d0
leeyuchong <leeyuchong@outlook.com>
2021-04-30 17:11:31 -0400

    Registrar warning

--- CourseBrowser/src/components/Search/SearchSection.js
+++ CourseBrowser/src/components/Search/SearchSection.js
@@ -8,23 +8,59 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import CloseIcon from '@material-ui/icons/Close';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 function SearchSection(props) {
     const [searchURL, setSearchURL] = useState("https://schedulebrewer.ml/api/search/?")
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [openAlert, setOpenAlert] = useState(true)
     const handleClickOpen = () => {
         setOpen(true);
       };
-      const handleClose = () => {
+    const handleClose = () => {
         setOpen(false);
-      };
-      const authLink = (event) => {
+    };
+    const authLink = (event) => {
         window.location.href = 'https://schedulebrewer.ml/saml/?sso'
-      }
+    }
+    const preventDefault = (event) => event.preventDefault();
     return (
         <div>
             <Box>
                 <SearchInputsContainer setSearchURL={setSearchURL}/>
+                <br/>
+                <Collapse in={openAlert}>
+                    <Alert 
+                        severity="warning"
+                        action={
+                            <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                setOpenAlert(false);
+                            }}
+                            >
+                            <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        }
+                    >
+                        <AlertTitle>
+                            <Typography variant="body2">
+                                ScheduleBrewer is a tool to help visualize various class schedules in preparation for pre-registration.
+                            </Typography>
+                        </AlertTitle>
+                        <Typography variant="caption">
+                            Saving a class does not mean you have registered for it. Remember to log in at <Link href="https://aisapps.vassar.edu/prereg/" onClick={preventDefault} style={{color: "#E88B00"}}>https://aisapps.vassar.edu/prereg/</Link> and formally register for your classes before preregistration ends.
+                        </Typography>
+                    </Alert>
+                </Collapse>
                 <br/>
                 <SearchResults 
                     searchURL={searchURL} 
@@ -50,7 +86,6 @@ function SearchSection(props) {
                     </Button>
                 </DialogActions>
             </Dialog>
-
         </div>
     )
 }
